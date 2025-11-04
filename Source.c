327bf4f53d46334f5e6e0753f870527a816fe1c5
kretininavera6-ux <kretininavera6@gmail.com>
2025-11-04 23:30:52 +0300

    Update Source.c

--- Source.c
+++ Source.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
-
 int main() {
-    int size = 10;
-    char symbol = 'ã';
-
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            printf("%c", symbol);
+    int i, j;
+    for (i = 0; i <= 4; i++) {
+        if (i == 0) {
+            printf("0\n");
+            continue;
+        }
+        for (j = i; j >= 1; j--) {
+            printf("%d ", j);
+        }
+        printf("0");
+        if (i == 3) {
+            printf(" 3");
+        }
+        else if (i == 4) {
+            printf(" 4 1");
         }
         printf("\n");
     }
-
     return 0;
 }
